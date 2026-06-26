eb7793ac4d37b9f2e9402aed81224cba1c635989
406-29-Naratip <54636@yupparaj.ac.th>
2026-06-26 10:32:49 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("☄My Digital Profile")
+st.header("🌟 แนะนำตัว")
+st.write("นาย นราธิป หน่อคำศักดิ์")
+st.write("ชื่อเล่น อาร์ตี้")
+st.write("ม.4/6 เลขที่ 29")
+st.header("🌕 เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย : อยากเรียนเก่งๆ")
+st.write("สิ่งที่อยากทำในอนาคต : ยูทูบเบอร์ (YouTuber)")
